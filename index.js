8ad938fddf7cb89d73cc308990ef63d192c941d4
coryellenberger <cory.ellen.ce@gmail.com>
2021-01-14 17:06:00 -0800

    Testing the Azure push deployment logic to confirm it will deploy changes from master(main)

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((request, response) => {
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.end("Hello World!");
+    response.end("This is how we do it!!!!");
 });
 
 const port = process.env.PORT || 3000;
